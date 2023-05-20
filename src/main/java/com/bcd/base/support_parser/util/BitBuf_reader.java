1706012aff3a3db00a3344b9ccc9a6b30f16e8c4
baichangda <471267877@qq.com>
2023-05-20 10:34:54 +0800

    c

--- src/main/java/com/bcd/base/support_parser/util/BitBuf_reader.java
+++ src/main/java/com/bcd/base/support_parser/util/BitBuf_reader.java
@@ -1,9 +1,16 @@
 package com.bcd.base.support_parser.util;
 
+import com.google.common.base.Strings;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BitBuf_reader {
+
+    static Logger logger = LoggerFactory.getLogger(BitBuf_reader.class);
+
     private final ByteBuf byteBuf;
     private byte b;
     //当前readIndex的bit偏移量
@@ -28,7 +35,7 @@ public class BitBuf_reader {
                 (byte) 0xF0, (byte) 0xe4,
                 (byte) 0xF0, (byte) 0xe4
         };
-        for (int i = 0; i < 100000000; i++) {
+        for (int i = 0; i < 1; i++) {
 //            ByteBuf bb = Unpooled.wrappedBuffer(source);
 //            BitBuf_reader bitBuf = BitBuf_reader.newBitBuf(bb);
 //            final long bitVal1 = bitBuf.read(1);
@@ -56,13 +63,19 @@ public class BitBuf_reader {
 
 
             final byte[] source2 = {
-                    (byte) 0x80, (byte) 0xe4,
+                    (byte) 0x81, (byte) 0x72,
             };
             ByteBuf bb2 = Unpooled.wrappedBuffer(source2);
             BitBuf_reader bitBuf2 = BitBuf_reader.newBitBuf(bb2);
-            final long l1 = bitBuf2.read(3);
-            final long l2 = bitBuf2.read(3);
-            final long l3 = bitBuf2.read(9);
+//            final long l1 = bitBuf2.read_log(3);
+//            final long l2 = bitBuf2.read_log(3);
+//            final long l3 = bitBuf2.read_log(9);
+            final LogRes res1 = bitBuf2.read_log(3);
+            final LogRes res2 = bitBuf2.read_log(3);
+            final LogRes res3 = bitBuf2.read_log(9);
+            res1.print();
+            res2.print();
+            res3.print();
 //            System.out.println(l1);
 //            System.out.println(l2);
 //            System.out.println(l3);
@@ -73,6 +86,65 @@ public class BitBuf_reader {
     public void finish() {
         b = 0;
         bitOffset = 0;
+    }
+
+
+    public static class LogRes {
+        public final byte[] bytes;
+
+        public final int bitStart;
+
+        public final int bitEnd;
+
+        public long val;
+
+        public LogRes(int byteLen, int bitStart, int bitEnd) {
+            this.bytes = new byte[byteLen];
+            this.bitStart = bitStart;
+            this.bitEnd = bitEnd;
+        }
+
+        public String getLogHex() {
+            return ByteBufUtil.hexDump(bytes) + ((bitEnd & 7) == 7 ? "" : "?");
+        }
+
+        public String getLogBit() {
+            final StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(Strings.padStart(Integer.toBinaryString(b & 0xff), 8, '0'));
+            }
+            return sb.substring(bitStart, bitEnd);
+        }
+
+        public void print() {
+            logger.info("read bit_hex[{}] bit_pos[{}-{}] bit_binary[{}] bit_val[{}]", getLogHex(), bitStart, bitEnd, getLogBit(), val);
+        }
+    }
+
+    public LogRes read_log(int bit) {
+        if (bitOffset == 0) {
+            b = byteBuf.readByte();
+        }
+        final int temp = bit + bitOffset;
+        final int byteLen = (temp >> 3) + ((temp & 7) == 0 ? 0 : 1);
+
+        final LogRes logRes = new LogRes(byteLen, bitOffset, bitOffset + bit-1);
+
+        logRes.bytes[0] = b;
+
+        long c = (b & 0xffL) << ((byteLen - 1) * 8);
+        for (int i = 1; i < byteLen; i++) {
+            b = byteBuf.readByte();
+            logRes.bytes[i] = b;
+            c |= (b & 0xffL) << ((byteLen - 1 - i) << 3);
+        }
+        final int right = byteLen * 8 - bitOffset - bit;
+
+        bitOffset = temp & 7;
+
+        logRes.val = (c >>> right) & ((0x01L << bit) - 1);
+
+        return logRes;
     }
 
     public long read(int bit) {
--- src/main/java/com/bcd/base/support_parser/util/BitBuf_writer.java
+++ src/main/java/com/bcd/base/support_parser/util/BitBuf_writer.java
@@ -1,10 +1,15 @@
 package com.bcd.base.support_parser.util;
 
+import com.google.common.base.Strings;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BitBuf_writer {
+
+    static Logger logger = LoggerFactory.getLogger(BitBuf_writer.class);
     private final ByteBuf byteBuf;
 
     private byte b;
@@ -22,7 +27,7 @@ public class BitBuf_writer {
 
     public static void main(String[] args) {
         final long t1 = System.currentTimeMillis();
-        for (int i = 0; i < 100000000; i++) {
+        for (int i = 0; i < 1; i++) {
 //            final ByteBuf bb = Unpooled.buffer();
 //            final BitBuf_writer bitBufWriter = BitBuf_writer.newBitBuf(bb);
 //            bitBufWriter.write(1L, 1);
@@ -52,11 +57,12 @@ public class BitBuf_writer {
 
             final ByteBuf bb = Unpooled.buffer();
             final BitBuf_writer bitBufWriter = BitBuf_writer.newBitBuf(bb);
-            bitBufWriter.write(4, 3);
-            bitBufWriter.write(0, 3);
-            bitBufWriter.write(114, 9);
-            bitBufWriter.write(114, 9);
-            bitBufWriter.write(114, 9);
+            final LogRes res1 = bitBufWriter.write_log(4, 3);
+            final LogRes res2 = bitBufWriter.write_log(0, 3);
+            final LogRes res3 = bitBufWriter.write_log(185, 9);
+            res1.print();
+            res2.print();
+            res3.print();
             bitBufWriter.finish();
 //            System.out.println(ByteBufUtil.hexDump(bb));
         }
@@ -69,6 +75,66 @@ public class BitBuf_writer {
         }
         b = 0;
         bitOffset = 0;
+    }
+
+    public static class LogRes {
+        public final byte[] bytes;
+
+        public final int bitStart;
+
+        public final int bitEnd;
+
+        public long val;
+
+        public LogRes(int byteLen, int bitStart, int bitEnd, long val) {
+            this.bytes = new byte[byteLen];
+            this.bitStart = bitStart;
+            this.bitEnd = bitEnd;
+            this.val = val;
+        }
+
+        public String getLogHex() {
+            return ByteBufUtil.hexDump(bytes) + ((bitEnd & 7) == 7 ? "" : "?");
+        }
+
+        public String getLogBit() {
+            final StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(Strings.padStart(Integer.toBinaryString(b & 0xff), 8, '0'));
+            }
+            return sb.substring(bitStart, bitEnd);
+        }
+
+        public void print() {
+            logger.info("write bit_val[{}] bit_binary[{}] bit_hex[{}] bit_pos[{}-{}]", val, getLogBit(), getLogHex(), bitStart, bitEnd);
+        }
+    }
+
+    public final LogRes write_log(long l, int bit) {
+        final int temp = bit + bitOffset;
+        final int byteLen = (temp >> 3) + ((temp & 7) == 0 ? 0 : 1);
+
+        final LogRes logRes = new LogRes(byteLen, bitOffset, bitOffset + bit - 1, l);
+
+        final int left = 8 - (temp & 7);
+        final long newL = l << left;
+        if (bitOffset == 0) {
+            b = (byte) (newL >> ((byteLen - 1) << 3));
+        } else {
+            b |= (byte) (newL >> ((byteLen - 1) << 3));
+        }
+        logRes.bytes[0] = b;
+        for (int i = 1; i < byteLen; i++) {
+            byteBuf.writeByte(b);
+            b = (byte) (newL >> ((byteLen - i - 1) << 3));
+            logRes.bytes[i] = b;
+        }
+        bitOffset = temp & 7;
+        if (bitOffset == 0) {
+            byteBuf.writeByte(b);
+        }
+
+        return logRes;
     }
 
     public final void write(long l, int bit) {
