406da16074327d2c230121c8e260ba76acce9d9c
baichangda <471267877@qq.com>
2023-05-22 10:27:26 +0800

    c

--- src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/processor/Packet_evts_processor.java
+++ src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/processor/Packet_evts_processor.java
@@ -27,66 +27,66 @@ public class Packet_evts_processor implements Processor<List<Evt>> {
             final int evtId = data.getUnsignedShort(data.readerIndex());
             final Evt evt;
             switch (evtId) {
-//                case 0x0001 -> {
-//                    evt = Parser.parse(Evt_0001.class, data, parentContext);
-//                }
-//                case 0x0004 -> {
-//                    evt = Parser.parse(Evt_0004.class, data, parentContext);
-//                }
-//                case 0x0005 -> {
-//                    evt = Parser.parse(Evt_0005.class, data, parentContext);
-//                }
-//                case 0x0006 -> {
-//                    evt = Parser.parse(Evt_0006.class, data, parentContext);
-//                }
-//                case 0x0007 -> {
-//                    evt = Parser.parse(Evt_0007.class, data, parentContext);
-//                }
-//                case 0x0008 -> {
-//                    evt = Parser.parse(Evt_0008.class, data, parentContext);
-//                }
-//                case 0x0009 -> {
-//                    evt = Parser.parse(Evt_0009.class, data, parentContext);
-//                }
-//                case 0x000A -> {
-//                    evt = Parser.parse(Evt_000A.class, data, parentContext);
-//                }
-//                case 0x0801 -> {
-//                    evt = Parser.parse(Evt_0801.class, data, parentContext);
-//                }
-//                case 0x0802 -> {
-//                    evt = Parser.parse(Evt_0802.class, data, parentContext);
-//                }
-//                case 0x0803 -> {
-//                    evt = Parser.parse(Evt_0803.class, data, parentContext);
-//                }
+                case 0x0001 -> {
+                    evt = Parser.parse(Evt_0001.class, data, parentContext);
+                }
+                case 0x0004 -> {
+                    evt = Parser.parse(Evt_0004.class, data, parentContext);
+                }
+                case 0x0005 -> {
+                    evt = Parser.parse(Evt_0005.class, data, parentContext);
+                }
+                case 0x0006 -> {
+                    evt = Parser.parse(Evt_0006.class, data, parentContext);
+                }
+                case 0x0007 -> {
+                    evt = Parser.parse(Evt_0007.class, data, parentContext);
+                }
+                case 0x0008 -> {
+                    evt = Parser.parse(Evt_0008.class, data, parentContext);
+                }
+                case 0x0009 -> {
+                    evt = Parser.parse(Evt_0009.class, data, parentContext);
+                }
+                case 0x000A -> {
+                    evt = Parser.parse(Evt_000A.class, data, parentContext);
+                }
+                case 0x0801 -> {
+                    evt = Parser.parse(Evt_0801.class, data, parentContext);
+                }
+                case 0x0802 -> {
+                    evt = Parser.parse(Evt_0802.class, data, parentContext);
+                }
+                case 0x0803 -> {
+                    evt = Parser.parse(Evt_0803.class, data, parentContext);
+                }
                 case 0xD006 -> {
                     evt = Parser.parse(Evt_D006.class, data, parentContext);
                 }
-//                case 0xD008 -> {
-//                    evt = Parser.parse(Evt_D008.class, data, parentContext);
-//                }
-//                case 0xD009 -> {
-//                    evt = Parser.parse(Evt_D009.class, data, parentContext);
-//                }
-//                case 0xD00A -> {
-//                    evt = Parser.parse(Evt_D00A.class, data, parentContext);
-//                }
-//                case 0xD00B -> {
-//                    evt = Parser.parse(Evt_D00B.class, data, parentContext);
-//                }
-//                case 0xD00C -> {
-//                    evt = Parser.parse(Evt_D00C.class, data, parentContext);
-//                }
-//                case 0xD00D -> {
-//                    evt = Parser.parse(Evt_D00D.class, data, parentContext);
-//                }
-//                case 0xD00E -> {
-//                    evt = Parser.parse(Evt_D00E.class, data, parentContext);
-//                }
-//                case 0xD00F -> {
-//                    evt = Parser.parse(Evt_D00F.class, data, parentContext);
-//                }
+                case 0xD008 -> {
+                    evt = Parser.parse(Evt_D008.class, data, parentContext);
+                }
+                case 0xD009 -> {
+                    evt = Parser.parse(Evt_D009.class, data, parentContext);
+                }
+                case 0xD00A -> {
+                    evt = Parser.parse(Evt_D00A.class, data, parentContext);
+                }
+                case 0xD00B -> {
+                    evt = Parser.parse(Evt_D00B.class, data, parentContext);
+                }
+                case 0xD00C -> {
+                    evt = Parser.parse(Evt_D00C.class, data, parentContext);
+                }
+                case 0xD00D -> {
+                    evt = Parser.parse(Evt_D00D.class, data, parentContext);
+                }
+                case 0xD00E -> {
+                    evt = Parser.parse(Evt_D00E.class, data, parentContext);
+                }
+                case 0xD00F -> {
+                    evt = Parser.parse(Evt_D00F.class, data, parentContext);
+                }
                 default -> {
                     if ((evtId >= 0x0001 && evtId <= 0x07FF)
                             || (evtId >= 0x0800 && evtId <= 0x0FFF)
