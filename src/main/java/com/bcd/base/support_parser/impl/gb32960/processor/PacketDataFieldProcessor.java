631a5d607b842049eefffd80c28d40fbd33a197e
baichangda <471267877@qq.com>
2023-06-10 10:07:27 +0800

    c

--- src/main/java/com/bcd/base/support_parser/impl/gb32960/processor/PacketDataFieldProcessor.java
+++ src/main/java/com/bcd/base/support_parser/impl/gb32960/processor/PacketDataFieldProcessor.java
@@ -11,60 +11,72 @@ public class PacketDataFieldProcessor implements Processor<PacketData> {
 
     @Override
     public PacketData process(final ByteBuf data, final ProcessContext parentContext) {
-        Packet packet = (Packet) parentContext.instance;
-        PacketData packetData = null;
-        switch (packet.flag) {
-            //车辆登入
-            case vehicle_login_data: {
-                packetData = Parser.parse(VehicleLoginData.class, data, parentContext);
-                break;
-            }
+        final Packet packet = (Packet) parentContext.instance;
+        if (packet.replyFlag == 0xfe) {
+            PacketData packetData = null;
+            switch (packet.flag) {
+                //车辆登入
+                case vehicle_login_data: {
+                    packetData = Parser.parse(VehicleLoginData.class, data, parentContext);
+                    break;
+                }
 
-            //车辆实时信息
-            case vehicle_run_data: {
-                packetData = Parser.parse(VehicleRunData.class, data, parentContext);
-                break;
-            }
+                //车辆实时信息
+                case vehicle_run_data: {
+                    packetData = Parser.parse(VehicleRunData.class, data, parentContext);
+                    break;
+                }
 
-            //补发信息上报
-            case vehicle_supplement_data: {
-                packetData = Parser.parse(VehicleSupplementData.class, data, parentContext);
-                break;
-            }
+                //补发信息上报
+                case vehicle_supplement_data: {
+                    packetData = Parser.parse(VehicleSupplementData.class, data, parentContext);
+                    break;
+                }
 
-            //车辆登出
-            case vehicle_logout_data: {
-                packetData = Parser.parse(VehicleLogoutData.class, data, parentContext);
-                break;
-            }
+                //车辆登出
+                case vehicle_logout_data: {
+                    packetData = Parser.parse(VehicleLogoutData.class, data, parentContext);
+                    break;
+                }
 
-            //平台登入
-            case platform_login_data: {
-                packetData = Parser.parse(PlatformLoginData.class, data, parentContext);
-                break;
-            }
+                //平台登入
+                case platform_login_data: {
+                    packetData = Parser.parse(PlatformLoginData.class, data, parentContext);
+                    break;
+                }
 
-            //平台登出
-            case platform_logout_data: {
-                packetData = Parser.parse(PlatformLogoutData.class, data, parentContext);
-                break;
-            }
+                //平台登出
+                case platform_logout_data: {
+                    packetData = Parser.parse(PlatformLogoutData.class, data, parentContext);
+                    break;
+                }
 
-            //心跳
-            case heartbeat: {
-                break;
-            }
+                //心跳
+                case heartbeat: {
+                    break;
+                }
 
-            //终端校时
-            case terminal_timing: {
-                break;
+                //终端校时
+                case terminal_timing: {
+                    break;
+                }
             }
+            return packetData;
+        } else {
+            final ResponseData responseData = new ResponseData();
+            responseData.content = new byte[packet.contentLength];
+            data.readBytes(responseData.content);
+            return responseData;
         }
-        return packetData;
     }
 
     @Override
     public void deProcess(ByteBuf data, ProcessContext parentContext, PacketData instance) {
-        Parser.deParse(instance, data, parentContext);
+        final Packet packet = (Packet) parentContext.instance;
+        if (packet.replyFlag == 0xfe) {
+            Parser.deParse(instance, data, parentContext);
+        } else {
+            data.writeBytes(((ResponseData) instance).content);
+        }
     }
 }
