537b11d0f127ec1bdbf0c753c7ea9239bea983f4
baichangda <471267877@qq.com>
2023-10-08 15:57:38 +0800

    Changes

--- src/main/java/com/bcd/base/support_parser/impl/gb32960/data/PacketData.java
+++ src/main/java/com/bcd/base/support_parser/impl/gb32960/data/PacketData.java
@@ -1,6 +1,8 @@
 package com.bcd.base.support_parser.impl.gb32960.data;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public interface PacketData {
 
 }
