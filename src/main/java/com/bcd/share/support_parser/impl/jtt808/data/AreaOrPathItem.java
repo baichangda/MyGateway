2f7824c996e6b3379dad7277520ffd12afdbd871
baichangda <471267877@qq.com>
2023-10-17 17:36:31 +0800

    Changes

--- src/main/java/com/bcd/share/support_parser/impl/jtt808/data/AreaOrPathItem.java
+++ src/main/java/com/bcd/share/support_parser/impl/jtt808/data/AreaOrPathItem.java
@@ -1,4 +1,7 @@
 package com.bcd.share.support_parser.impl.jtt808.data;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public interface AreaOrPathItem {
 }
