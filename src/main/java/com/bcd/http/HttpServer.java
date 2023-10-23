e76e88ed9e2977f1ec723d97e89d1c181d8a9c6a
baichangda <471267877@qq.com>
2023-10-23 18:08:52 +0800

    Changes

---
+++ src/main/java/com/bcd/http/HttpServer.java
@@ -0,0 +1,13 @@
+package com.bcd.http;
+
+import io.jooby.Jooby;
+
+
+public class HttpServer {
+    public static void main(String[] args) {
+        Jooby.runApp(args, app -> {
+
+        });
+    }
+
+}
