e1342c994a812183edf6657f1972db93519a153b
baichangda <471267877@qq.com>
2023-12-21 11:36:10 +0800

    Changes

--- src/main/java/com/bcd/share/support_parser/anno/F_bit_skip.java
+++ src/main/java/com/bcd/share/support_parser/anno/F_bit_skip.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface F_bit_skip {
+public @interface F_bit_skip{
     /**
      * 占用bit位
      */
--- src/main/java/com/bcd/share/support_parser/impl/immotors/ep33/data/Evt_0801.java
+++ src/main/java/com/bcd/share/support_parser/impl/immotors/ep33/data/Evt_0801.java
@@ -1,6 +1,6 @@
 package com.bcd.share.support_parser.impl.immotors.ep33.data;
 
-import com.bcd.share.support_parser.anno.F_bit_num;
+import com.bcd.share.support_parser.anno.F_num;
 import com.bcd.share.support_parser.anno.F_skip;
 import com.bcd.share.support_parser.anno.NumType;
 import com.bcd.share.support_parser.impl.immotors.Evt_2_6;
@@ -8,6 +8,6 @@ import com.bcd.share.support_parser.impl.immotors.Evt_2_6;
 public class Evt_0801 extends Evt_2_6 {
     @F_skip(len = 5)
     public byte skip;
-    @F_bit_num(len = 8,  valExpr = "x*0.392157")
+    @F_num(type = NumType.uint8,  valExpr = "x*0.392157")
     public float BrkPdlPos;
 }
