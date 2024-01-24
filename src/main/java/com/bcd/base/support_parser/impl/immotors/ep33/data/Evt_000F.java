f8c313a6d6c4ff5fa09cd7a0f1089e493a7ce64f
baichangda <471267877@qq.com>
2024-01-24 14:57:19 +0800

    c

--- src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/data/Evt_000F.java
+++ src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/data/Evt_000F.java
@@ -8,6 +8,6 @@ import com.bcd.base.support_parser.impl.immotors.Evt_2_6;
 public class Evt_000F extends Evt_2_6 {
     @F_num(type = NumType.uint16, valExpr = "x*0.1-2000")
     public float TMActuToqHiPre;
-    @F_bit_num(len = 15, valExpr = "x*0.1-2000")
+    @F_bit_num(len = 15, valExpr = "x*0.1-1000")
     public float TMInvtrCrntHiPre;
 }
