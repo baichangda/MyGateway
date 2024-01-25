6ed6fd71859384c0a0f59ccf3f313e7988f786b9
baichangda <471267877@qq.com>
2024-01-25 09:28:44 +0800

    c

--- src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/data/Evt_D00E.java
+++ src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/data/Evt_D00E.java
@@ -1,13 +1,14 @@
 package com.bcd.base.support_parser.impl.immotors.ep33.data;
 
-import com.bcd.base.support_parser.anno.F_bit_num_array;
 import com.bcd.base.support_parser.anno.F_num;
+import com.bcd.base.support_parser.anno.F_num_array;
+import com.bcd.base.support_parser.anno.F_string;
 import com.bcd.base.support_parser.anno.NumType;
 import com.bcd.base.support_parser.impl.immotors.Evt_4_x;
 
 public class Evt_D00E extends Evt_4_x {
     @F_num(type = NumType.uint8, var = 'a')
     public short BMSRptBatCodeNum;
-    @F_bit_num_array(lenExpr = "a", singleValType = NumType.uint8, singleLen = 8)
-    public byte[] BMSRptBatCodeAsc;
+    @F_string(lenExpr = "a")
+    public String BMSRptBatCodeAsc;
 }
