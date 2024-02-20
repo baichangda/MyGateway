60eebefbb07880da0e5648389df4c333211d6dcb
baichangda <471267877@qq.com>
2024-02-20 15:49:58 +0800

    c

--- src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/data/Evt_D00E.java
+++ src/main/java/com/bcd/base/support_parser/impl/immotors/ep33/data/Evt_D00E.java
@@ -9,6 +9,6 @@ import com.bcd.base.support_parser.impl.immotors.Evt_4_x;
 public class Evt_D00E extends Evt_4_x {
     @F_num(type = NumType.uint8, var = 'a')
     public short BMSRptBatCodeNum;
-    @F_string(lenExpr = "a")
-    public String BMSRptBatCodeAsc;
+    @F_num_array(singleType = NumType.uint8,lenExpr = "a")
+    public byte[] BMSRptBatCodeAsc;
 }
