639b6cc052161cbfc02d27a94acf1655c50e7738
baichangda <471267877@qq.com>
2024-06-11 17:45:19 +0800

    c

--- src/main/java/com/bcd/base/support_parser/anno/C_impl.java
+++ src/main/java/com/bcd/base/support_parser/anno/C_impl.java
@@ -19,7 +19,7 @@ public @interface C_impl {
      * 当表达式解析为此值时候
      * 表明其实现类为当前注解标注的类
      *
-     * 特殊值{@link Integer#MAX_VALUE}代表、当所有分支不命中时候、使用此注解标注的类
+     * 特殊值{@link Integer#MAX_VALUE}代表默认分支、当所有分支不命中时候、使用此注解标注的类
      */
     int[] value();
 
