d50875c3e5405d5268b623a744247f54275cd82f
baichangda <471267877@qq.com>
2024-06-28 15:38:28 +0800

    c

--- src/main/java/com/bcd/base/support_parser/impl/gb32960/data/ResponseData.java
+++ src/main/java/com/bcd/base/support_parser/impl/gb32960/data/ResponseData.java
@@ -1,5 +1,8 @@
 package com.bcd.base.support_parser.impl.gb32960.data;
 
+import com.bcd.base.support_parser.anno.C_impl;
+
+@C_impl(value = Integer.MAX_VALUE)
 public class ResponseData implements PacketData {
     public byte[] content;
 }
