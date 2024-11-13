9fb16c3d37a597379d4298fd35d88717109b7b42
baichangda <471267877@qq.com>
2024-11-13 15:53:28 +0800

    c

--- src/main/java/com/bcd/base/exception/BaseException.java
+++ src/main/java/com/bcd/base/exception/BaseException.java
@@ -1,21 +1,61 @@
 package com.bcd.base.exception;
 
+import com.bcd.base.util.ExceptionUtil;
 import com.bcd.base.util.StringUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * 建造此异常类的目的:
- * 1、在所有需要抛非运行时异常的地方,用此异常包装,避免方法调用时候需要捕获异常(若是其他框架自定义的异常,请不要用此类包装)
+ * 1、在所有需要抛非运行时异常的地方,用此异常包装,避免方法调用时候需要捕获异常
+ *    此时打印堆栈会打印完整的调用链、包括包装异常的调用链
+ *    此时异常信息为null、如果需要获取真实异常信息、调用{@link #getRealExceptionMessage()}
  * 2、在业务需要出异常的时候,定义异常并且抛出
  */
 public class BaseException extends RuntimeException {
     public int code = 1;
 
+    private final Throwable target;
+
     private BaseException(String message) {
         super(message);
+        this.target = null;
     }
 
-    private BaseException(Throwable e) {
-        super(e);
+    private BaseException(Throwable target) {
+        super(null, (Throwable) null);
+        this.target = target;
+    }
+
+    private BaseException(String message, Throwable target) {
+        super(message, (Throwable) null);
+        this.target = target;
+    }
+
+    public Throwable getTargetException() {
+        return target;
+    }
+
+    @Override
+    public Throwable getCause() {
+        return target;
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+
+    public String getRealExceptionMessage(){
+        return ExceptionUtil.getRealException(this).getMessage();
+    }
+
+
+    public BaseException code(int code) {
+        this.code = code;
+        return this;
     }
 
     public static BaseException get(String message) {
@@ -44,17 +84,45 @@ public class BaseException extends RuntimeException {
         return new BaseException(StringUtil.format(message, arg1, arg2));
     }
 
+    public static BaseException get(String message, Throwable e) {
+        return new BaseException(message, e);
+    }
+
     public static BaseException get(Throwable e) {
         return new BaseException(e);
     }
 
-    public static void main(String[] args) {
-        throw BaseException.get("[{}]-[{}]", null, 100000);
-    }
+    static Logger logger = LoggerFactory.getLogger(BaseException.class);
 
-    public BaseException code(int code) {
-        this.code = code;
-        return this;
+    public static void main(String[] args) {
+//        throw BaseException.get("[{}]-[{}]", null, 100000);
+
+        try {
+            String s = null;
+            s.getBytes();
+        } catch (Exception e) {
+            BaseException e1 = BaseException.get(e);
+            BaseException e2 = BaseException.get(e1);
+            logger.error("error", e2);
+            logger.info(e2.getMessage());
+        }
+
+
+        BaseException e = BaseException.get("测试");
+        BaseException e1 = BaseException.get(e);
+        BaseException e2 = BaseException.get(e1);
+        logger.error("error", e2);
+        logger.info(e2.getMessage());
+
+        InvocationTargetException e3 = new InvocationTargetException(e);
+        BaseException e4 = BaseException.get(e3);
+        logger.error("error", e4);
+        logger.info(e4.getMessage());
+
+        BaseException e5 = BaseException.get(e);
+        InvocationTargetException e6 = new InvocationTargetException(e5);
+        logger.error("error", e6);
+        logger.info(e6.getMessage());
     }
 
 }
---
+++ src/main/java/com/bcd/base/util/ExceptionUtil.java
@@ -0,0 +1,68 @@
+package com.bcd.base.util;
+
+import com.bcd.base.exception.BaseException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Created by Administrator on 2017/7/27.
+ */
+public class ExceptionUtil {
+
+    private final static Logger logger = LoggerFactory.getLogger(ExceptionUtil.class);
+
+    /**
+     * 获取堆栈信息
+     *
+     * @param throwable
+     * @return
+     */
+    public static String getStackTraceMessage(Throwable throwable) {
+        if (throwable == null) {
+            return "";
+        }
+        try (StringWriter sw = new StringWriter();
+             PrintWriter pw = new PrintWriter(sw)) {
+            throwable.printStackTrace(pw);
+            return sw.toString();
+        } catch (IOException e) {
+            throw BaseException.get(e);
+        }
+    }
+
+
+    /**
+     * 获取真实的异常
+     *
+     * @param throwable
+     * @return
+     */
+    public static Throwable getRealException(Throwable throwable) {
+        if (throwable == null) {
+            return null;
+        }
+        Throwable temp = throwable;
+        while (true) {
+            if (temp instanceof BaseException ex) {
+                if (ex.getTargetException() == null) {
+                    return temp;
+                } else {
+                    temp = ex.getTargetException();
+                }
+            } else if (temp instanceof InvocationTargetException ex) {
+                if (ex.getTargetException() == null) {
+                    return temp;
+                } else {
+                    temp = ex.getTargetException();
+                }
+            } else {
+                return temp;
+            }
+        }
+    }
+}
